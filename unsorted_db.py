9987c87d1f53d029d1c90c904bc8a55b3dcc2d02
TheFakeNatty <colin.ripley@outlook.com>
2023-04-03 00:48:47 -0400

    Unsorted MongoDB Push

---
+++ unsorted_db.py
@@ -0,0 +1,30 @@
+import json
+import os
+import pymongo
+from pymongo import MongoClient
+
+db_url= "mongodb://localhost:27017/"
+myclient = MongoClient(db_url)
+db = myclient["Unsorted"]
+collection = db['IoT_Devices']
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+data_folder = os.path.join(current, 'data')
+
+
+def main():
+    print(data_folder)
+    for folder in os.listdir(data_folder):
+        if folder == 'README.md':
+            pass
+        else:
+            for file in os.listdir(str(data_folder) + "\\" + folder):
+                f = open(str(data_folder) + "\\" + folder + "\\" + file)
+                data = json.load(f)
+
+                for value in data:
+                    collection.insert_one(value)
+
+if __name__ in "__main__":
+    main()
