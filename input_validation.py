e3f4fa44122c3feb8322849a301d13717bd2b3c6
Colin Ripley <colinripley@mac-studio.myfiosgateway.com>
2023-04-06 18:33:53 -0400

    Updating path variable to make OS agnostic

--- input_validation.py
+++ input_validation.py
@@ -3,11 +3,9 @@ import os
 import time
 from pathlib import Path
 
-current = os.path.dirname(os.path.realpath(__file__))
- 
-parent = os.path.dirname(current)
- 
-data_folder = os.path.join(current, 'Data')
+currentpath = Path(__file__)
+rootpath = Path(currentpath).parent
+data_folder = Path(rootpath / "data")
 
 def print_file(file_path):
     f = open(file_path)
@@ -21,8 +19,8 @@ def main():
         if folder == 'README.md':
             pass
         else: 
-            for file in os.listdir(str(data_folder) + "\\" + folder):
-                print_file(str(data_folder) + "\\" + folder + "\\" + file)
+            for file in os.listdir(Path(data_folder / folder)):
+                print_file(Path(data_folder / folder / file))
 
 if __name__ == "__main__":
     main()
