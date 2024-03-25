46a3e369dfbee02a5e2d650c641cb4b9c0f402b5
TheFakeNatty <cgeek94@gmail.com>
2024-03-24 20:54:36 -0400

    Updating functions to handle null values.
    Previous versions did not implement anything for null values or errors.

--- utils/db_ops.py
+++ utils/db_ops.py
@@ -45,7 +45,7 @@ def unsorted_grouped_push(url, dbname, db_collection, data_folder):
                 for value in data:
                     collection.insert_one(value)
 
-def sorted_push(url, dbname, db_collection, data_folder):
+def sorted_unoptimized_push(url, dbname, db_collection, data_folder):
     db_client = MongoClient(url)
     db = db_client[dbname]
     collection = db[db_collection]
@@ -179,7 +179,7 @@ def sorted_push(url, dbname, db_collection, data_folder):
                             except:
                                 print("Failed to push value" + v)
 
-def sorted_grouped_push(url, dbname, db_collection, data_folder):
+def sorted_unoptimized_grouped_push(url, dbname, db_collection, data_folder):
     db_client = MongoClient(url)
     db = db_client[dbname]
     collection = db[db_collection]
@@ -233,7 +233,7 @@ def sorted_grouped_push(url, dbname, db_collection, data_folder):
                     except:
                         value["ID"] = ""
                     try:
-                        value["TIME"] = float(value["TIME"])
+                        value["TIME"] = value["TIME"]
                     except:
                         value["TIME"] = ""
                     try:
@@ -320,6 +320,190 @@ def sorted_grouped_push(url, dbname, db_collection, data_folder):
                                     })
                             except:
                                 print("Failed to push value" + v)
+
+def sorted_optimized_grouped_push(url, dbname, db_collection, data_folder):
+    db_client = MongoClient(url)
+    db = db_client[dbname]
+    collection = db[db_collection]
+    ts = datetime.datetime.now()
+    for folder in os.listdir(data_folder):
+        if folder == "README.md":
+            pass
+        else:
+            for file in os.listdir(Path(data_folder / folder)):
+                try:
+                    collection_name = str(file.split('.')[0])
+                    db.create_collection(collection_name, timeseries={ 'timeField': 'TIME', 'metaField': 'metadata' })
+                except:
+                    print("Collection Already Exists...")
+
+    for folder in os.listdir(data_folder):
+        if folder == 'README.md':
+            pass
+        else:
+            for file in os.listdir(Path( data_folder / folder)):
+                f = open(Path(data_folder / folder / file))
+                data = json.load(f)
+
+                collection_name = str(file.split('.')[0])
+                print(collection_name)
+                collection = db[collection_name]
+                
+                for value in data:
+                    try:
+                        time_value = value["TIME"]
+                        # Convert string to datetime object
+                        dt = datetime.datetime.strptime(time_value, "%Y-%m-%d %H:%M:%S.%f")
+
+                        # Round microseconds to three decimal places
+                        rounded_microseconds = round(dt.microsecond, -3)
+
+                        # Create a new datetime object with rounded microseconds
+                        rounded_dt = dt.replace(microsecond=rounded_microseconds)
+
+                        # Format datetime object to desired format
+                        formatted_time = rounded_dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-4]
+
+                        formatted_time = formatted_time + "+00:00"
+
+                        formatted_time = datetime.datetime.strptime(formatted_time, "%Y-%m-%dT%H:%M:%S.%f%z")
+                    except:
+                        formatted_time = datetime.datetime.now()
+
+                    try:
+                        value["ID"] = value["ID"]
+                    except:
+                        value["ID"] = ""
+                    try:
+                        value["TIME"] = value["TIME"]
+                    except:
+                        value["TIME"] = None
+                    try:
+                        value["TMP"] = float(value["TMP"])
+                    except:
+                        value["TMP"] = None
+                    try:
+                        value["OPT"] = float(value["OPT"])
+                    except:
+                        value["OPT"] = None
+                    try:
+                        value["BAT"] = float(value["BAT"])
+                    except:
+                        value["BAT"] = None
+                    try:
+                        value["HDT"] = float(value["HDT"])
+                    except:
+                        value["HDT"] = None
+                    try:
+                        value["BAR"] = float(value["BAR"])
+                    except:
+                        value["BAR"] = None
+                    try:
+                        value["HDH"] = float(value["HDH"])
+                    except:
+                        value["HDH"] = None
+
+                    if value["TIME"] == None:
+                        pass
+                    else:
+                        collection.insert_one({
+                            "metadata": {
+                                "ID": value["ID"], 
+                                "type": "EnviroSensor",
+                            },
+                            "TIME": formatted_time,
+                            "reading": {
+                                key: value[key] for key in ["TMP", "OPT", "BAT", "HDT", "BAR", "HDH"] if value[key] is not None
+                            }
+                        })
+
+def sorted_optimized_push(url, dbname, db_collection, data_folder):
+    db_client = MongoClient(url)
+    db = db_client[dbname]
+    collection = db[db_collection]
+
+    try:
+        db.create_collection(db_collection, timeseries={ 'timeField': 'TIME', 'metaField': 'metadata' })
+    except:
+        print("Either the collection already exists of something failed...")
+
+    print(f"Pushing Sorted data to {dbname}, collection {db_collection}.....")
+
+    for folder in os.listdir(data_folder):
+        if folder == 'README.md':
+            pass
+        else:
+            for file in os.listdir(Path( data_folder / folder)):
+                f = open(Path(data_folder / folder / file))
+                data = json.load(f)
+                
+                for value in data:
+                    try:
+                        time_value = value["TIME"]
+                        # Convert string to datetime object
+                        dt = datetime.datetime.strptime(time_value, "%Y-%m-%d %H:%M:%S.%f")
+
+                        # Round microseconds to three decimal places
+                        rounded_microseconds = round(dt.microsecond, -3)
+
+                        # Create a new datetime object with rounded microseconds
+                        rounded_dt = dt.replace(microsecond=rounded_microseconds)
+
+                        # Format datetime object to desired format
+                        formatted_time = rounded_dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-4]
+
+                        formatted_time = formatted_time + "+00:00"
+
+                        formatted_time = datetime.datetime.strptime(formatted_time, "%Y-%m-%dT%H:%M:%S.%f%z")
+                    except:
+                        formatted_time = datetime.datetime.now()
+
+                    try:
+                        value["ID"] = value["ID"]
+                    except:
+                        value["ID"] = ""
+                    try:
+                        value["TIME"] = value["TIME"]
+                    except:
+                        value["TIME"] = None
+                    try:
+                        value["TMP"] = float(value["TMP"])
+                    except:
+                        value["TMP"] = None
+                    try:
+                        value["OPT"] = float(value["OPT"])
+                    except:
+                        value["OPT"] = None
+                    try:
+                        value["BAT"] = float(value["BAT"])
+                    except:
+                        value["BAT"] = None
+                    try:
+                        value["HDT"] = float(value["HDT"])
+                    except:
+                        value["HDT"] = None
+                    try:
+                        value["BAR"] = float(value["BAR"])
+                    except:
+                        value["BAR"] = None
+                    try:
+                        value["HDH"] = float(value["HDH"])
+                    except:
+                        value["HDH"] = None
+
+                    if value["TIME"] == None:
+                        pass
+                    else:
+                        collection.insert_one({
+                            "metadata": {
+                                "ID": value["ID"], 
+                                "type": "EnviroSensor",
+                            },
+                            "TIME": formatted_time,
+                            "reading": {
+                                key: value[key] for key in ["TMP", "OPT", "BAT", "HDT", "BAR", "HDH"] if value[key] is not None
+                            }
+                        })
 
 if __name__ == "__main__":
     pass
